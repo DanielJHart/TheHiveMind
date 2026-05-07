e4a659a9c1baf4e2bdba288d427ce5f019ea8a4d
DanielJHart <Daniel.j.hart1995@gmail.com>
2026-05-07 07:17:55 +0100

    Adding upgraded relic Universal Conscious

---
+++ TheHiveMindCode/Patches/Patch_TouchOfOrobas.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Relics;
+using TheHiveMind.TheHiveMindCode.Relics;
+
+namespace TheHiveMind.TheHiveMindCode.Patches;
+
+// This solution taken from https://github.com/YuWan886/Sts2-YuWanCard/blob/59293432afe58c52678f36bb57297298eff80872/YuWanCardCode/Patches/TouchOfOrobasPatch.cs#L9
+
+[HarmonyPatch(typeof(TouchOfOrobas), nameof(TouchOfOrobas.AfterObtained))]
+public static class Patch_TouchOfOrobas
+{
+    [HarmonyPostfix]
+    [HarmonyPatch("RefinementUpgrades", MethodType.Getter)]
+    public static void AddPigCarrotUpgrade(ref Dictionary<ModelId, RelicModel> __result)
+    {
+        var relic = ModelDb.Relic<UniversalMind>();
+        var upgradedRelic = ModelDb.Relic<UniversalConscious>();
+        
+        if (!__result.ContainsKey(relic.Id))
+        {
+            __result[relic.Id] = upgradedRelic;
+        }
+    }
+}
---
+++ TheHiveMindCode/Relics/UniversalConscious.cs
@@ -0,0 +1,24 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.Rewards;
+using MegaCrit.Sts2.Core.Rooms;
+using TheHiveMind.TheHiveMindCode.Character;
+using TheHiveMind.TheHiveMindCode.Relics;
+
+namespace TheHiveMind.TheHiveMindCode.Relics;
+
+[Pool(typeof(TheHiveMindRelicPool))]
+public class UniversalConscious() : UniversalMind
+{
+    public override RelicRarity Rarity => RelicRarity.Starter;
+
+    public override bool TryModifyRewardsLate(Player player, List<Reward> rewards, AbstractRoom? room)
+    {
+        if (player != this.Owner)
+            return false;
+        foreach (CardReward cardReward in rewards.OfType<CardReward>())
+            cardReward.CanReroll = true;
+        return true;
+    }
+}
